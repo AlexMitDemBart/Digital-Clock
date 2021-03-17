d6d5690004792d2e9c89378ec7bc140d66803e55
Alexander Zotz <AlexMitDemBart@gmail.com>
2021-03-17 08:39:39 +0100

    added date to the digital clock

--- script.js
+++ script.js
@@ -2,12 +2,13 @@
 getCurrentTime()
 
 function getCurrentTime() {
-    const currentDate = new Date()
+    let currentDate = new Date()
 
     const currentTime = formatTime(currentDate.getHours()) + ":"  + formatTime(currentDate.getMinutes()) + ":" 
     + formatTime(currentDate.getSeconds());
 
     setTime(currentTime)
+    setCurrentDate()
 }
 
 function setTime(time){
@@ -15,6 +16,18 @@ function setTime(time){
     timeElement.innerText = time
 }
 
+function setCurrentDate(){
+    const dateElement = document.getElementById('date')
+    
+    let today = new Date();
+    let dd = String(today.getDate()).padStart(2, '0');
+    let mm = String(today.getMonth() + 1).padStart(2, '0');
+    let yyyy = today.getFullYear();
+
+    today = dd + '.' + mm + '.' + yyyy;
+
+    dateElement.innerText = today
+}
 
 function formatTime(time) {
   return time < 10 ? `0${time}` : time
